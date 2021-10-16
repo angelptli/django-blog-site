1af78ce8e41a85a2765d88261cea8000d753883c
angelptli <ali95@mail.ccsf.edu>
2021-10-15 19:26:55 -0700

    Add style to blog posts and home page

--- myblog/models.py
+++ myblog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="My Blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
