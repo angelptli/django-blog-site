f29a30d796031a45a9e04354700f57092546cf40
angelptli <ali95@mail.ccsf.edu>
2021-10-15 20:07:00 -0700

    Stylize add post form

---
+++ myblog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter a Blog Title'
+                }),
+            'title_tag': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter a Title Tag'
+                }),
+            'author': forms.Select(attrs={
+                'class': 'form-control'
+                }),
+            'body': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Type Your Post'
+                }),
+        }
--- myblog/views.py
+++ myblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # List query sets and view in detail retrieved db info
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # def home(request):
@@ -16,8 +17,10 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
 
+
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    # fields = '__all__'
     # fields = ('title', 'body')
