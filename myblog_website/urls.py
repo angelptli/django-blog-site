316906f86a065a734fe6d02692d27edb99b0445b
angelptli <ali95@mail.ccsf.edu>
2021-10-15 21:28:53 -0700

    Add user login authentication

--- myblog_website/urls.py
+++ myblog_website/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Point to myblog app
     path('', include('myblog.urls')),
+
+    # Django user authentication package
+    path('members/', include('django.contrib.auth.urls')),
+    # Point to members app
+    path('members/', include('members.urls')),
 ]
