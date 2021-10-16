8f8e22e7ce6b08f4c2a92d5dd81dac1b3393ebb5
angelptli <ali95@mail.ccsf.edu>
2021-10-15 21:08:44 -0700

    Order blog posts by most current date

--- myblog/views.py
+++ myblog/views.py
@@ -14,7 +14,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    ordering = ['-id']
+    ordering = ['-post_date']
 
 
 class ArticleDetailView(DetailView):
