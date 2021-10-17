451ac809be89782ddf3d770d110389bb9bf2cd43
angelptli <ali95@mail.ccsf.edu>
2021-10-16 19:46:16 -0700

    Add category pages

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 # from . import views
 from .views import AddPostView, HomeView, ArticleDetailView, AddPostView
 from .views import UpdatePostView, DeletePostView, AddCategoryView
+from .views import CategoryView
 
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
-    path('add_category', AddCategoryView.as_view(), name="add_category")
+    path('add_category/', AddCategoryView.as_view(), name="add_category"),
+    path('category/<str:cats>/', CategoryView, name="category"),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -17,6 +17,15 @@ class HomeView(ListView):
     ordering = ['-post_date']
 
 
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+
+    return render(request, 'categories.html', {
+        'cats': cats,
+        'category_posts': category_posts
+        })
+
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
