3bcd6a4e82cabb3ccf2634e609a26c5bea91b210
angelptli <ali95@mail.ccsf.edu>
2021-10-17 19:18:32 -0700

    Create an Unlike button

--- myblog/views.py
+++ myblog/views.py
@@ -14,7 +14,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
 
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
@@ -63,6 +70,13 @@ class ArticleDetailView(DetailView):
         total_likes = stuff.total_likes()
         context["total_likes"] = total_likes
 
+        liked = False
+
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context["liked"] = liked
+        
         return context
 
 
