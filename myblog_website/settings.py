ef547b6411c59ed9ea4e9cea21229fef7569fc58
angelptli <ali95@mail.ccsf.edu>
2021-10-19 14:03:32 -0700

    Add default profile pic using static files

--- myblog_website/settings.py
+++ myblog_website/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,6 +126,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
